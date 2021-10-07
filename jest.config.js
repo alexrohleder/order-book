a35a0cdc3bb8336f28de7739fc31c73fb797853a
Alex Rohleder <a.rohleder@sportradar.com>
2021-10-07 21:14:35 +0200

    setup tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+};
